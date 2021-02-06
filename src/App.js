506f6f10caa4654bd16aa7cf7eaef060ec669f83
Roni Scott <scot0410@d.umn.edu>
2021-02-06 15:12:38 -0600

    included emulator state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import ReactTerminal from 'react-terminal-component';
+import Terminal from 'react-terminal-component'
+import {
+  EmulatorState, OutputFactory, Outputs
+} from 'javascript-terminal';
+
 import './App.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,13 +23,24 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function App() {
+  const defaultState = EmulatorState.createEmpty();
+  const defaultOutputs = defaultState.getOutputs();
+
+  const newOutputs = Outputs.addRecord(
+    defaultOutputs, OutputFactory.makeTextOutput(
+      `This is an example of using output to display a message
+to the user before any commands are run.`
+    )
+  );
+  const emulatorState = defaultState.setOutputs(newOutputs);
+
 
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <ReactTerminal emulatorState={emulatorState}/>
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>xs=6</Paper>
