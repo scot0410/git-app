4d1cdd820bca90fd6694d55db634bfac10f530c7
Roni Scott <scot0410@d.umn.edu>
2021-02-06 15:32:26 -0600

    added custom command functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import ReactTerminal from 'react-terminal-component';
 import Terminal from 'react-terminal-component'
 import {
-  EmulatorState, OutputFactory, Outputs
+  EmulatorState, OutputFactory, Outputs, FileSystem, CommandMapping, defaultCommandMapping
 } from 'javascript-terminal';
 
 import './App.css';
@@ -23,16 +23,30 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function App() {
-  const defaultState = EmulatorState.createEmpty();
-  const defaultOutputs = defaultState.getOutputs();
+  const customState = EmulatorState.create({
+    'commandMapping': CommandMapping.create({
+      ...defaultCommandMapping,
+      'print': {
+        'function': (state, opts) => {
+          const input = opts.join(' ');
 
-  const newOutputs = Outputs.addRecord(
-    defaultOutputs, OutputFactory.makeTextOutput(
-      `This is an example of using output to display a message
-to the user before any commands are run.`
-    )
-  );
-  const emulatorState = defaultState.setOutputs(newOutputs);
+          return {
+            output: OutputFactory.makeTextOutput(input)
+          };
+        },
+        'optDef': {}
+      }
+    }),
+    'fs': FileSystem.create({
+      '/home': { },
+      '/home/README': {content: 'This is a text file'},
+      '/home/nested/directory': {},
+      '/home/nested/directory/file': {content: 'End of nested directory!'}
+    })
+  });
+  // const defaultState = EmulatorState.createEmpty();
+  // const defaultOutputs = defaultState.getOutputs();
+  // const emulatorState = defaultState.setOutputs(newOutputs);
 
 
   const classes = useStyles();
@@ -40,7 +54,10 @@ to the user before any commands are run.`
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={6}>
-          <ReactTerminal emulatorState={emulatorState}/>
+        <ReactTerminal
+          inputStr=''
+          emulatorState={customState}
+        />
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>xs=6</Paper>
